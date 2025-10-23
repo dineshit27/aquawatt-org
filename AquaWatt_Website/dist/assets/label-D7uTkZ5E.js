f7a3c23e95689f56a4819ebe872e7ed647bfa9e5
dineshit27 <m.dinesh.it27@gmail.com>
2025-10-23 19:32:41 +0530

    push Aquawatt_Website

---
+++ AquaWatt_Website/dist/assets/label-D7uTkZ5E.js
@@ -0,0 +1,6 @@
+import{c,r as s,j as o,P as n,e as d,ae as m}from"./index-pyDnZmym.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=c("Lock",[["rect",{width:"18",height:"11",x:"3",y:"11",rx:"2",ry:"2",key:"1w4ew1"}],["path",{d:"M7 11V7a5 5 0 0 1 10 0v4",key:"fwvmzm"}]]);var f="Label",i=s.forwardRef((a,t)=>o.jsx(n.label,{...a,ref:t,onMouseDown:e=>{var r;e.target.closest("button, input, select, textarea")||((r=a.onMouseDown)==null||r.call(a,e),!e.defaultPrevented&&e.detail>1&&e.preventDefault())}}));i.displayName=f;var l=i;const u=m("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),x=s.forwardRef(({className:a,...t},e)=>o.jsx(l,{ref:e,className:d(u(),a),...t}));x.displayName=l.displayName;export{x as L,y as a};
---
+++ src/pages/Billing.tsx
@@ -0,0 +1,6 @@
+// This file was a duplicate of the main app's Billing page and is intentionally
+// neutralized to avoid confusion. The authoritative Billing page lives at:
+//   waterlight-harmony-07-main/src/pages/Billing.tsx
+// This module exports nothing and is not used by the application build.
+
+export {};
